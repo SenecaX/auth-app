739e52dc261491945b0d6ca3cf79f4515e72e331
Kedaren Dorasami <kedaren.dorasami@elca.mu>
2020-06-15 08:41:03 +0400

    added jwt token

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const ctrlAuth = require("../controllers/authentication");
+const jwt = require("express-jwt");
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: "payload"
+});
 
 router.post("/register", ctrlAuth.register);
 router.post("/login", ctrlAuth.login);
